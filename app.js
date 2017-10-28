826cdc8de9d23d310fcbdd606b47daeeab7f25cb
rjtj2007 <teacherman1@gmail.com>
2017-10-28 13:56:20 -0700

    corrected html, added css, starting into js

---
+++ app.js
@@ -0,0 +1,45 @@
+'use strict';
+//
+// //array for object storage
+// Catalog.allItems = [];
+//
+// //object with constructor function
+// function Items(name, filepath) {
+//   this.name = name;
+//   this.filepath = filepath;
+//   Catalog.allItems.push(this);
+// }
+//
+// // //make new instance of items for above function
+// new Item('bag', 'images/bag.jpg');
+// new Item('banana', 'images/banana.jpg');
+// new Item('bathroom', 'images/bathroom.jpg');
+// new Item('boots', 'images/boots.jpg');
+// new Item('breakfast', 'images/breakfast.jpg');
+// new Item('bubblegum', 'images/bubblegum.jpg');
+// new Item('chair', 'images/chair.jpg');
+// new Item('thing', 'images/cthulhu.jpg');
+// new Item('duck-dog', 'images/duck-dog.jpg');
+// new Item('dragon', 'images/dragon.jpg');
+// new Item('pen', 'images/pen.jpg');
+// new Item('pet-sweep', 'images/pet-sweep.jpg');
+// new Item('scissors', 'images/scissors.jpg');
+// new Item('shark', 'images/shark.jpg');
+// new Item('sweep', 'images/sweep.png');
+// new Item('tauntaun', 'images/tauntaun.jpg');
+// new Item('unicorn', 'images/unicorn.jpg');
+// new Item('usb', 'images/usb.gif');
+// new Item('water-can', 'images/water-can.jpg');
+// new Item('wine-glass', 'images/wine-glass.jpg');
+//
+// // //event listener
+// var imgEl = document.getElementById('');
+// // var imgEl = document.getElementById('goat-pic');
+// //
+// // imgEl.addEventListener('click', randomGoat);
+// // //random display of the pictures
+// // function randomGoat() {
+// //   var randomIndex = Math.floor(Math.random() * Goat.allGoats.length);
+// //   imgEl.src = Goat.allGoats[randomIndex].filepath;
+// // }
+// // randomGoat();
