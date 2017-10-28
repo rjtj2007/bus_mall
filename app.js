8822695f48c45b2489f765594025720c1feba8df
rjtj2007 <teacherman1@gmail.com>
2017-10-28 14:55:44 -0700

    adding random images display

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 'use strict';
-
+console.log('Good Morning!');
 //array for object storage
-Catalog.allItems = [];
+var allItem = [];
 
 //object with constructor function
-function Items(name, filepath) {
+function Item(name, filepath) {
   this.name = name;
   this.filepath = filepath;
-  Catalog.allItems.push(this);
+  allItem.push(this);
 }
 
 // //make new instance of items for above function
@@ -33,13 +33,17 @@ new Item('water-can', 'images/water-can.jpg');
 new Item('wine-glass', 'images/wine-glass.jpg');
 
 // //event listener
-var imgEl = document.getElementById('');
-// var imgEl = document.getElementById('goat-pic');
+var imgEl = document.getElementById('Item');
+imgEl.addEventListener('click', randomItem);
+
+//display picture randomly
+function randomItem() {
+  var randomIndex = Math.floor(Math.random() * allItem.length);
+  return allItem[randomIndex].filepath;
+}
+randomItem();
+
 //
-// imgEl.addEventListener('click', randomGoat);
-// //random display of the pictures
-// function randomGoat() {
-//   var randomIndex = Math.floor(Math.random() * Goat.allGoats.length);
-//   imgEl.src = Goat.allGoats[randomIndex].filepath;
-// }
-// randomGoat();
+var leftImg.src = itemArray[randomItem()].filePath;
+var centerImg.src = itemArray[randomItem()].filePath;
+var rightImg.src = itemArray[randomItem()].filePath;
