38eee70aaa7ef9dd1af4aaf67bbabf037acc9653
rjtj2007 <teacherman1@gmail.com>
2017-11-04 18:42:14 -0700

    list displays, clicks and shown tracker functional

--- app.js
+++ app.js
@@ -2,17 +2,10 @@
 
 var allItem = [];
 var queue = [];
-// var catalogue = document.getElementById('catalogue');
 var image1 = document.getElementById('image1');
 var image2 = document.getElementById('image2');
 var image3 = document.getElementById('image3');
 var totalClicks = 0;
-// var status = document.getElementById('status');
-// var holdingArray = [];
-// var counter = 0;
-// var pageTotalClicks = 0;
-// var votes = [];
-// var imgIds = [];
 
 //constructor funciton
 function Item(name, filePath) {
@@ -51,7 +44,6 @@ makeItem();
 //use for all instances of random number
 function randomNumber() {
   return Math.floor(Math.random() * 20);
-  console.log('run some random numbers');
 }
 
 for (var i = 0; i < 6; i++) {
@@ -103,144 +95,59 @@ function make3Images() {
 make3Images();
 
 if (totalClicks > 3) {
-  image1.removeEventListener('click', image1Click);
-  image2.removeEventListener('click', image2Click);
   image3.removeEventListener('click', image3Click);
 }
 
 //add event listener for 1 2 3
 function image1Click() {
-  allItem[queue[3]].clicked++;
-  totalClicks++;
-  console.log('image 1 ' + allItem[queue[3]].name + allItem[queue[3]].clicked);
-  random3Numbers();
-  make3Images();
-  console.log('totalClicks ' + totalClicks);
+  if (totalClicks > 3) {
+    image1.removeEventListener('click', image1Click);
+    makeList();
+  } else {
+    allItem[queue[3]].clicked++;
+    totalClicks++;
+    console.log('image 1 ' + allItem[queue[3]].name + allItem[queue[3]].clicked);
+    random3Numbers();
+    make3Images();
+    console.log('totalClicks ' + totalClicks);
+  }
 }
 image1.addEventListener('click', image1Click);
 
 function image2Click() {
-  allItem[queue[4]].clicked++;
-  totalClicks++;
-  console.log('image 2 ' + allItem[queue[4]].name + allItem[queue[4]].clicked);
-  random3Numbers();
-  make3Images();
+  if (totalClicks > 3) {
+    image2.removeEventListener('click', image2Click);
+    makeList();
+  } else {
+    allItem[queue[4]].clicked++;
+    totalClicks++;
+    console.log('image 2 ' + allItem[queue[4]].name + allItem[queue[4]].clicked);
+    random3Numbers();
+    make3Images();
+  }
 }
 image2.addEventListener('click', image2Click);
 
 function image3Click() {
-  allItem[queue[5]].clicked++;
-  totalClicks++;
-  console.log('image 3 ' + allItem[queue[5]].name + allItem[queue[5]].clicked);
-  random3Numbers();
-  make3Images();
+  if (totalClicks > 3) {
+    image3.removeEventListener('click', image3Click);
+    makeList();
+  } else {
+    allItem[queue[5]].clicked++;
+    totalClicks++;
+    console.log('image 3 ' + allItem[queue[5]].name + allItem[queue[5]].clicked);
+    random3Numbers();
+    make3Images();
+  }
 }
 image3.addEventListener('click', image3Click);
 
-//
-// function totalClicks1() {
-//   holdingArray[randomIndex1].clicks++;
-//   pageTotalClicks++;
-//   console.log(pageTotalClicks);
-//   executeImages();
-//   makeList();
-// }
-//
-// function totalClicks2() {
-//   holdingArray[randomIndex2].clicks++;
-//   pageTotalClicks++;
-//   console.log(pageTotalClicks);
-//   executeImages();
-//   makeList();
-// }
-//
-// function totalClicks3() {
-//   holdingArray[randomIndex3].clicks++;
-//   pageTotalClicks++;
-//   console.log(pageTotalClicks);
-//   executeImages();
-//   makeList();
-// }
-//
-// function randomImage1() {
-//   var randomIndex1 = randomNumber();
-//   if (pageTotalClicks > 24) {
-//     imgEl1.removeEventListener('click', totalClicks1);
-//   }
-//   var previousIndex1 = holdingArray[0];
-//   previousIndex1 = catalogueImages.indexOf(previousIndex1);
-//
-//   var previousIndex2 = holdingArray[1];
-//   previousIndex2 = catalogueImages.indexOf(previousIndex2);
-//
-//   var previousIndex3 = holdingArray[2];
-//   previousIndex3 = catalogueImages.indexOf(previousIndex3);
-//
-//   while (randomIndex1 === previousIndex1 || randomIndex1 === previousIndex2 || randomIndex1 === previousIndex3) {
-//     randomIndex1 = Math.floor(Math.random() * catalogueImages.length);
-//   }
-//   holdingArray[0].shown += 1;
-//   imgEl1.src = catalogueImages[randomIndex1].filePath;
-// }
-//
-// function randomImage2() {
-//   if (pageTotalClicks > 24) {
-//     imgEl2.removeEventListener('click', totalClicks2);
-//   }
-//   var previousIndex1 = holdingArray[0];
-//   var previousIndex1 = catalogueImages.indexOf(previousIndex2);
-//
-//   var previousIndex2 = holdingArray[1];
-//   previousIndex2 = catalogueImages.indexOf(previousIndex2);
-//
-//   var previousIndex3 = holdingArray[2];
-//   previousIndex3 = catalogueImages.indexOf(previousIndex3);
-//
-//   while (randomIndex2 === randomIndex1 || randomIndex2 === previousIndex1 || randomIndex2 === previousIndex2 || randomIndex2 === previousIndex3) {
-//     randomIndex2 = Math.floor(Math.random() * catalogueImages.length);
-//   }
-//   holdingArray[1].shown += 1;
-//   ingEl2.src = catalogueImages[randomIndex2].filePath;
-// }
-//
-// function randomImage3() {
-//   if (pageTotalClicks > 24) {
-//     imgEl3.removeEventListener('click', totalClicks3);
-//   }
-//   var previousIndex1 = holdingArray[0];
-//   var previousIndex1 = catalogueImages.indexOf(previousIndex2);
-//
-//   var previousIndex2 = holdingArray[1];
-//   previousIndex2 = catalogueImages.indexOf(previousIndex2);
-//
-//   var previousIndex3 = holdingArray[2];
-//   previousIndex3 = catalogueImages.indexOf(previousIndex3);
-//
-//   while (randomIndex3 === randomIndex1 || randomIndex3 === randomIndex2 || randomIndex3 === previousIndex1 || randomIndex3 === previousIndex2 || randomIndex3 === previousIndex3) {
-//
-//     imgEl3.src = holdingArray[randomIndex3].filePath;
-//     catalogueImages[randomIndex3].shown += 1;
-//   }
-// }
-//
-// // function executeImages() {
-// //   randomImage1();
-// //   randomImage2();
-// //   randomImage3();
-// //   previousIndex1 = randomIndex1;
-// //   previousIndex2 = randomIndex2;
-// //   previousIndex3 = randomIndex3;
-// // }
-// // executeImages();
-//
-// // function makeList() {
-// //   if (pageTotalClicks === 25) {
-// //     console.log('test');
-// //     var ulEl = document.getElementById('list');
-// //     for (var i = 0; i < holdingArray.length; i++) {
-// //       var liEl = document.createElement(li);
-// //       liEl.textContent = holdingArray[i].name + 'was selected ' + holdingArray[i].totalClicks + ' times.';
-// //       ulEl.appendChild(liEl);
-// //     }
-// //   }
-// // }
+function makeList() {
+  console.log('test');
+  var ulEl = document.getElementById('listResults');
+  for (var i = 0; i < allItem.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = allItem[i].name + ' was displayed ' + allItem[i].shown + ' was clicked ' + allItem[i].clicked + ' times.';
+    ulEl.appendChild(liEl);
+  }
+}
